7df73d8765022e35dcc872941765fb97bb11f428
Bui Quoc Trieu <buiquoctrieu0301@gmail.com>
2021-01-12 16:43:13 +1100

    done review

---
+++ .history/js/main_20210112155750.js
@@ -0,0 +1,44 @@
+// Service Carousel
+$(".service__carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  dots: false,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 5,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
+
+// CAROUSEL 2
+$(".review__carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  autoplay: false,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true,
+  // responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
